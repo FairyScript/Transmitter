83014cd14123580fb9203d5778ca5cd93c2f5780
FairyScript <h@queb.fun>
2021-09-12 03:13:24 +0800

    init commit

---
+++ Transmitter/Plugin.cs
@@ -0,0 +1,63 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using System.Reflection;
+
+namespace SamplePlugin
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "Sample Plugin";
+
+        private const string commandName = "/pmycommand";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        private Configuration Configuration { get; init; }
+        private PluginUI PluginUi { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] CommandManager commandManager)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            this.PluginUi = new PluginUI(this.Configuration);
+
+            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+
+        public void Dispose()
+        {
+            this.PluginUi.Dispose();
+            this.CommandManager.RemoveHandler(commandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.PluginUi.Visible = true;
+        }
+
+        private void DrawUI()
+        {
+            this.PluginUi.Draw();
+        }
+
+        private void DrawConfigUI()
+        {
+            this.PluginUi.SettingsVisible = true;
+        }
+    }
+}
